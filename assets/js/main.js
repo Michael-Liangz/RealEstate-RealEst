4763b705a1e4fa05eea469bd1b06ec845c5b8cc0
Michael-Liangz <Michaelliangz.ml@gmail.com>
2023-01-07 18:27:41 +0700

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 // TODO ======================= CHANGE BACKGROUND HEADER =======================
 function scrollHeader() {
   const header = document.getElementById("header");
+  // ? When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
   if (this.scrollY >= 50) {
     header.classList.add("scroll-header");
   } else {
@@ -9,9 +10,19 @@ function scrollHeader() {
 }
 window.addEventListener("scroll", scrollHeader);
 
-// ? When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-
 // TODO ======================= SWIPER POPULAR =======================
+// ! Source code dari SwiperJs, https://codesandbox.io/s/5vdwrg?file=/index.html:2253-2418 -->
+var swiperPopular = new Swiper(".popular_container", {
+  spaceBetween: 32,
+  grabCursor: true,
+  centerdSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 // TODO ======================= VALUE ACCORDION =======================
 
