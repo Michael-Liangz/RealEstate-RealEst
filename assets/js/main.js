5e0b8f71a27b4c866a1a49000e644e1ee628f195
Michael-Liangz <Michaelliangz.ml@gmail.com>
2023-01-07 15:59:49 +0700

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,15 @@
 // TODO ======================= CHANGE BACKGROUND HEADER =======================
+function scrollHeader() {
+  const header = document.getElementById("header");
+  if (this.scrollY >= 50) {
+    header.classList.add("scroll-header");
+  } else {
+    header.classList.remove("scroll-header");
+  }
+}
+window.addEventListener("scroll", scrollHeader);
+
+// ? When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
 
 // TODO ======================= SWIPER POPULAR =======================
 
